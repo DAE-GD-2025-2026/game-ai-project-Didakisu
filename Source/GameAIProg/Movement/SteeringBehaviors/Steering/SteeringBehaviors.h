ae1f39c544ba36da407a65dde16f7e06369591aa
Dimana Nedeva Radeva <dimana_n_radeva@abv.bg>
2026-02-09 11:49:26 +0100

    Started doing Seek Behavior

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.h
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.h
@@ -26,3 +26,13 @@ protected:
 };
 
 // Your own SteeringBehaviors should follow here...
+
+class Seek : public ISteeringBehavior
+{
+public:
+	Seek() = default;
+	virtual ~Seek() = default;
+
+	//steering
+	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) override;
+};
